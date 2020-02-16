6823e904c4366e41224d8edb140a296ce73a2939
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-02-16 15:56:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -331,7 +331,7 @@ async def on_message(message):
     rannum = random.randrange(20)
     if rannum == 1:
         ch = client.get_channel(676812476561489921)
-        await ch.send(random.randrange(10**100))
+        await ch.send(f"{message.author}『{message.content}』")
 
 
 @client.event

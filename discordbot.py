57a6fe7138d5550c2fde8781f79dc3c1959785c5
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-02-27 01:05:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,6 +43,59 @@ kisei_flag = False
 do_time = 0
 
 @client.event
+async def on_ready():
+    log_ch = client.get_channel(676505024435585055)
+    await log_ch.send('```起動ログ\n{datetime.now(JST)}```')
+    loop.start()
+
+
+@tasks.loop(seconds = 40):
+async def loop:
+    global test_flag
+    global test_ch
+    global SSR_flag
+    global check_flag
+    global stop_num
+    tao = client.get_user(526620171658330112)
+
+
+    if test_flag==True and SSR_flag == False and check_flag == False:
+        check_flag = True
+        if tao :
+            check_m1 = await test_ch.send('```Checking......```')
+            
+            if 1 == 1:
+                def test_check (tao_msg):
+                    if tao_msg.author != tao:
+                        return 0
+                    if tao_msg.channel!=test_ch:
+                        return 0
+                    return 1
+
+                try:
+                    t_res=await client.wait_for('message', timeout=10, check = test_check)
+                    
+
+                except asyncio.TimeoutError:
+                    await check_m1.edit(content = '```Checked```')
+                    stop_num+=1
+                    a = await test_ch.send(f'::attack')
+                    await a.edit(content = "```I tried to check for Auto Battle System\nAnd it isn't active!!( 'ω')ｷﾞｬｧｧｧｧｧｧ" + datetime.now(JST).strftime("\n`%Y/%m/%d %H:%M:%S` ```"))
+                 
+                else:
+                    await check_m1.edit(content = '```Checked```')
+                    await test_ch.send(("```I tried to check for Auto Battle System\nAnd it is active!!⸜(* ॑꒳ ॑*  )⸝" + datetime.now(JST).strftime("\n`%Y/%m/%d %H:%M:%S` ```")))
+
+                check_flag = False
+                
+
+    now = datetime.now(JST).strftime('%H:%M')
+    if now == '00:00':
+        channel = client.get_channel(676499145208627201)
+        await channel.send('::login')  
+
+
+@client.event
 async def on_message(message):
     me = client.user
     amano = discord.utils.get(message.guild.members,id=446610711230152706)
@@ -74,7 +127,31 @@ async def on_message(message):
 
     sent = "None"
 
- 
+
+    if not atk_num == 0:       
+        sent1 = f"**現在ノ討伐数：**`{m_num}`"
+
+        if not R == 0:
+            sent2 = f"**Ｒ　　出現数：**`{R}({(round((R/m_num)*10000))/100}％)`"
+        else:
+            sent2 = f"**Ｒ　　出現数：**`{R}(0%)`"
+
+        if not SR == 0:
+            sent3 = f"**ＳＲ　出現数：**`{SR}({(round((SR/m_num)*10000))/100}％)`"
+        else:
+            sent3 = f"**ＳＲ　出現数：**`{SR}(0%)`"
+
+        if not SSR == 0:
+            sent4 = f"**ＳＳＲ出現数：**`{SSR}({(round((SSR/m_num)*10000))/100}％)`"
+        else:
+            sent4 = f"**ＳＳＲ出現数：**`{SSR}(0%)`"
+
+        sent5 = f"**総ダメージ数：**`{all_damage}({(round((all_damage)/(atk_num)))}/atk)`"
+        sent6 = f"**総獲得経験値：**`{all_exp}`"
+        sent7 = f'**総合ＬｖＵＰ：**`{lv}`'
+
+        sent = f'\n{sent1}\n{sent2}\n{sent3}\n{sent4}\n{sent5}\n{sent6}\n{sent7}'
+
 
     if message.author == me:
 
@@ -114,29 +191,6 @@ async def on_message(message):
     if message.content.startswith('a)prest') and not message.author.bot:
         await message.channel.send(f'{sent}')
 
-    if not atk_num == 0:       
-        sent1 = f"**現在ノ討伐数：**`{m_num}`"
-
-        if not R == 0:
-            sent2 = f"**Ｒ　　出現数：**`{R}({(round((R/m_num)*10000))/100}％)`"
-        else:
-            sent2 = f"**Ｒ　　出現数：**`{R}(0%)`"
-
-        if not SR == 0:
-            sent3 = f"**ＳＲ　出現数：**`{SR}({(round((SR/m_num)*10000))/100}％)`"
-        else:
-            sent3 = f"**ＳＲ　出現数：**`{SR}(0%)`"
-
-        if not SSR == 0:
-            sent4 = f"**ＳＳＲ出現数：**`{SSR}({(round((SSR/m_num)*10000))/100}％)`"
-        else:
-            sent4 = f"**ＳＳＲ出現数：**`{SSR}(0%)`"
-
-        sent5 = f"**総ダメージ数：**`{all_damage}({(round((all_damage)/(atk_num)))}/atk)`"
-        sent6 = f"**総獲得経験値：**`{all_exp}`"
-        sent7 = f'**総合ＬｖＵＰ：**`{lv}`'
-
-        sent = f'\n{sent1}\n{sent2}\n{sent3}\n{sent4}\n{sent5}\n{sent6}\n{sent7}'
     """
     if message.content == 'a)i m' and not message.author.bot;
         async with message.channel.typing():
@@ -151,11 +205,12 @@ async def on_message(message):
             if message.author==me or message.author.guild_permissions.administrator:
                 test_flag=False
                 test_ch=None
-                asent = f"\n**現在ノ討伐数**\n`{m_num}`"
+                asent =  f"\n**現在ノ討伐数**\n`{m_num}`"
                 asent += f"\n**停止検知回数**\n`{stop_num}`"
                 asent += f"\n**死亡復活回数**\n`{revive_num}`"
                 asent += f"\n**Ｒ　　出現数**\n`{R}`"
-                asent += f"\n**ＳＲ　出現数**\n`{SSR}`"
+                asent += f"\n**ＳＲ　出現数**\n`{SR}`"
+                asent += f"\n**ＳＳＲ出現数**\n`{SSR}`"
                 asent += f"\n**総ダメージ数**\n`{all_damage}`"
                 asent += f"\n**単発平均火力**\n`{(round((all_damage)/(atk_num)))}`"
                 asent += f"\n**総獲得経験値**\n`{all_exp}`"
@@ -299,7 +354,7 @@ async def on_message(message):
                     await asyncio.sleep(10)
                     await test_ch.send('COUNTDOWN\n__0__')
                     SSR_flag = False
-                    if not "狂気ネコしろまる" in message.embeds[0].title:
+                    if not "フロスト" in message.embeds[0].title:
                         await test_ch.send(f"::item f")
                         fb_flag = True
                     else:
@@ -342,42 +397,6 @@ async def on_message(message):
         else:
             await m_ch.send("**Couldn't Found The User**")
 
-    if test_flag==True and SSR_flag == False and check_flag == False:
-        check_flag = True
-        if tao :
-            check_m = '```Checking......```'
-            check_m1 = await test_ch.send(check_m)
-            
-            #if not(isinstance(tao.activity, discord.Game) and '::help' in tao.activity.name):
-                #await check_m1.edit(content = '```Checked```')
-                #await test_ch.send(("```I tried to check for Auto Battle System\nBut Tao wasn't active...(;´д｀)" + datetime.now(JST).strftime("\n%Y/%m/%d %H:%M:%S```")))
-                #await asyncio.sleep(30)
-                #check_flag = False
-                
-            if 1 == 1:
-                def test_check (d_msg):
-                    if d_msg.author != tao:
-                        return 0
-                    if d_msg.channel!=test_ch:
-                        return 0
-                    return 1
-
-                try:
-                    t_res=await client.wait_for('message', timeout=20, check = test_check)
-                    
-
-                except asyncio.TimeoutError:
-                    await check_m1.edit(content = '```Checked```')
-                    stop_num+=1
-                    a = await test_ch.send(f'::attack')
-                    await a.edit(content = "```I tried to check for Auto Battle System\nAnd it isn't active!!( 'ω')ｷﾞｬｧｧｧｧｧｧ" + datetime.now(JST).strftime("\n`%Y/%m/%d %H:%M:%S` ```"))
-                 
-                else:
-                    await check_m1.edit(content = '```Checked```')
-                    await test_ch.send(("```I tried to check for Auto Battle System\nAnd it is active!!⸜(* ॑꒳ ॑*  )⸝" + datetime.now(JST).strftime("\n`%Y/%m/%d %H:%M:%S` ```")))
-
-                check_flag = False
-                
             
     if message.content == 'a)check' :
         await message.channel.send(f'check_flag = {check_flag}')        
@@ -386,6 +405,14 @@ async def on_message(message):
     if message.content == '::item f' and message.author == client.user:
         await message.edit(content = '**スペルカード発動！**')
     
+    """
+    if message.content == a)history and message.author == me :
+        msgs = await message.channel.history( limit = 5 ).flatten()
+        for msg in msgs:
+            if msg.author == tao and msg.embeds and msg.embeds[0].title:
+                if '待ち構えている' in msg.embeds[0].title:
+                    ALL_m = msg
+    """
 
 
 @client.event

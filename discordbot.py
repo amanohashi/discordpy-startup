36bf17b8178b89825d1def44a0614e4b950400ff
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-03-10 15:14:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -71,7 +71,7 @@ async def loop():
     now_2 = datetime.now(JST).strftime('%m/%d %H:%M')
     
     f_ch = client.get_channel(686484146343641134)
-    await f_ch.send("t!fish")
+    #await f_ch.send("t!fish")
     
     if stop_skd or start_skd:
         print(f"{now_2} ≠ {stop_skd}")

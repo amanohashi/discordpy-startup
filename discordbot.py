d05774a10c8bb133207f8894ffcfe4270ae50bd0
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-03-11 18:01:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -57,7 +57,7 @@ async def on_ready():
 
 @tasks.loop(seconds=31)
 async def loop():
-    pint('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬')
+    print('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬')
     if ready == True:
         global test_flag
         global test_ch
@@ -70,10 +70,7 @@ async def loop():
         tao = client.get_user(526620171658330112)
         now = datetime.now(JST).strftime('%H:%M')
         now_2 = datetime.now(JST).strftime('%m/%d %H:%M')
-      
-        f_ch = client.get_channel(686484146343641134)
-        #await f_ch.send("t!fish")
-        pint(now)
+        print(now)
     
         if stop_skd or start_skd:
             print(f"{now_2} ≠ {stop_skd}")

d4d5a81c6e0f773639a88dec012aca716774374f
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2020-03-12 10:31:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1814,8 +1814,6 @@ async def on_message(message):
         for msg in msgs:
             msgem = msg.embeds[0].description
             for match in re.finditer('【(.+?)】',msgem):
-                if match[1] in j_list:
-                    return
                 j_list.append(match[1])
         J_list = list(set(j_list))     
         genres = '||,||'.join(j_list)

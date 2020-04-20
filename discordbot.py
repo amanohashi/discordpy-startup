e89dc9f6f114f7498779002d47c68a6db432167c
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-04-20 18:12:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -227,7 +227,7 @@ async def on_message(message):
             user_dic[user.id] = 1
         print(f"{user.name}:{user_dic[user.id]}")
         if user_dic[user.id] >= 10:
-            ch_id = 674983698080202797
+            ch_id = 701721786592657461
             ch = client.get_channel(ch_id)
             await ch.send(f"t!credit {user.mention} {user_dic[user.id]}")
             def check(msg):

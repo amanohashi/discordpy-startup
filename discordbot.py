304bc2c43acc27a6c806d4cc6ed5386149936e3d
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-05-13 11:43:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -264,11 +264,8 @@ async def on_message(message):
                     if MSG_EMBED.description:
                         print(MSG_EMBED.description)
                         em=discord.Embed(description="a")
-                        try:
-                            await message.channel.send(embed=em)
-                            await message.channel.send(MSG_EMBED.description)
-                        except:
-                            pass
+                        await message.channel.send(embed=em)
+
 
 
         
